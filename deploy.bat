0f79a288fd2aa4d0143b028d2eec0deb870c5218
Nguyen Văn Cong <congnv@etc.vn>
2024-07-22 08:41:01 +0700

    update

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,6 @@ echo todo.cocode.pro > CNAME
 git init
 git add -A
 git commit -m "deploy"
-git push -f https://github.com/nguyenvancongdev/web-vue.git master:gh-pages
+git push -f https://github.com/nguyenvancongdev/todo.git master:gh-pages
 pause
 echo .
